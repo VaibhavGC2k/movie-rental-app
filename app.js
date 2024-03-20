4be2bf5761119687335e9fc2ea9f3f0e8f398982
Vaibhav Chowdhary <86661832+VaibhavGC2k@users.noreply.github.com>
2024-03-20 09:36:46 +0000

    hey

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ require("./db");
 const errorHandler = require('./middlewares/error')
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4500
 
-const static_path = path.join(__dirname,"/public")
-const template_path = "D:/Movie_Rental_Project/templates/views"
-const partial_path = "D:/Movie_Rental_Project/templates/partials"
+const static_path = path.join("/workspaces/movie-rental-app/public")
+const template_path = "/workspaces/movie-rental-app/templates/views"
+const partial_path = "/workspaces/movie-rental-app/templates/partials"
 
 const registers = require('./routes/registerRoute');
 const login = require('./routes/loginRoute');
@@ -27,23 +27,23 @@ app.use(errorHandler)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index')
 })
 
 app.post('/logout', (req, res) => {
     res.clearCookie('token');
     res.redirect('/');
-  });
-  
-
-app.use('/register',registers)
-app.use('/login',login)
-app.use('/genre',genres)
-app.use('/movie',movies)
-app.use('/rental',rentals)
+});
 
 
-app.listen(PORT,()=>{
-    console.log('LIstening on port ',`http://localhost:${PORT}`)
+app.use('/register', registers)
+app.use('/login', login)
+app.use('/genre', genres)
+app.use('/movie', movies)
+app.use('/rental', rentals)
+
+
+app.listen(PORT, () => {
+    console.log('LIstening on port ', `http://localhost:${PORT}`)
 })
--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-module.exports = mongoose.connect('mongodb://127.0.0.1:27017/Movie_Rental')
-.then(()=>{
-    console.log('DB Connection Successful')
-}).catch((error)=>{
-    console.log('DB Connection Failed!!..',error.message)
-})  
+module.exports = mongoose.connect('mongodb://localhost:27017/Movie_Rental')
+    .then(() => {
+        console.log('DB Connection Successful')
+    }).catch((error) => {
+        console.log('DB Connection Failed!!..', error.message)
+    })  
